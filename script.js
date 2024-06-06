561cfb8ed8973509789e82b98250f1b3c712e703
subina <subinaghimire@gmail.com>
2024-06-06 13:13:29 +1000

    allow only one .

---
+++ script.js
@@ -0,0 +1,83 @@
+const allButtonsElm = document.querySelectorAll(".btn");
+
+let strToDisplay = "";
+const displayElm = document.querySelector(".display");
+
+const operators = ["%", "/", "*", "-", "+"];
+
+let lastOperator = "";
+
+const buttonAction = (value) => {
+    console.log(value);
+
+    if (value === "AC"){
+        strToDisplay = "";
+        return display(strToDisplay);
+    }
+
+    if (value === "C"){
+        strToDisplay = strToDisplay.slice(0, -1);
+        return display(strToDisplay);
+    }
+
+    if (value === "=") {
+  //get the last char
+  const lastChar = strToDisplay[strToDisplay.length - 1];
+
+  //check if it is one of the operators
+  if (operators.includes(lastChar)){
+    strToDisplay = strToDisplay.slice(0, -1);
+  }
+        return displayTotal();
+    }
+
+    // show only last clicked operator
+    if (operators.includes (value)){
+        lastOperator = value;
+        const lastChar = strToDisplay[strToDisplay.length - 1];
+
+        if (operators.includes(lastChar)){
+            strToDisplay = strToDisplay.slice(0, -1);
+        }
+    }
+
+    //handle the dot clicked
+
+    if (value === "."){
+        const lastOperatorIndex = strToDisplay.lastIndexOf(lastOperator);
+
+        const lastNumberSet = strToDisplay.slice(lastOperatorIndex);
+
+        if (lastNumberSet.includes(".")){
+            return;
+        }
+        
+        if (!lastOperator && strToDisplay.includes(".")){
+            return;
+        }
+    }
+    strToDisplay += value;
+
+        display(strToDisplay);
+
+}
+
+//attached click event to all the buttons
+allButtonsElm.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const value = btn.innerText;
+        buttonAction(value);
+    });
+});
+
+//update clicked button value to display area
+const display = (str) => {
+    displayElm.innerText = str || "0.0";
+};
+
+//calculate total
+const displayTotal = () => {
+    const total = eval(strToDisplay);
+    strToDisplay = total.toString();
+   display(total);
+}
